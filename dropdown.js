645f2ae1911deb4a44f2a0e90c2097a264e01554
Teresa Tang <tst37@cornell.edu>
2021-01-04 16:00:36 -0600

    dropdown for cat + opacity change

---
+++ dropdown.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  var highest = 1;
+
+  for (i = 1; i <= highest; i++) {
+    $("#d" + i).hide();
+  }
+
+  $(".drop").click(function () {
+    var num = $(this).attr('id');
+    toggleDrop(num);
+  });
+
+  function toggleDrop(num) {
+    $("#d" + num).toggle();
+  }
+
+});
