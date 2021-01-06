8af7d87a2ffaae05ccd57dc03ca495293079e346
Teresa Tang <tst37@cornell.edu>
2021-01-06 15:26:00 -0600

    made js execute before php

--- form.js
+++ form.js
@@ -26,7 +26,10 @@ $(document).ready(function () {
   // Cannot validate with required keyword in HTML for hidden elements, as this
   // causes weird issues with page submission and browser element not focusable
   // error
-  $("#Form").on("submit", function () {
+  $("#Form").submit(function (e) {
+    // Force Javascript to execute before submission to php 
+    e.preventDefault();
+
     valid = true;
     var form_type = $('input[type=radio]:checked', '#Form').val();
     var adopt = ["catid", "date1", "date2", "time1", "time2"];
